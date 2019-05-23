404af2920c1d2b823274ea68afb667ccb69cee0d
Aleksandra <ajmpawlik@gmail.com>
2019-05-24 00:15:49 +0100

    Wrapped up in methods and working - interactive menu

--- directory.rb
+++ directory.rb
@@ -1,34 +1,61 @@
+@students = []
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  name = gets.chomp
+  while !name.empty? do
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    name = gets.chomp
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu()
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      students = input_students
+    when "2"
+      show_students
+    when "9"
+      exit #this will casue the program to terminate
+    else
+      puts "I don't know what you meant, try again."
+  end
+end
+
 def print_header
 puts "The students of Villans Academy"
 puts "_____________"
 end
 
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  students = []
-  name = gets.chomp
-  while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
-  end
-  students
-end
-
-students = input_students
-#nothing happens until we call the methods
-print_header
-print(students)
-print_footer(students)
+interactive_menu()
